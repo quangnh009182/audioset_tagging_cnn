14f1b28342f8637f2d5473ed0d7eee0f84b63848
unknown <quang009182@gmail.com>
2021-10-15 16:35:38 +0700

    add customized code for audio tagging on an audio shot

---
+++ audio_tagging.py
@@ -0,0 +1,90 @@
+import os
+import sys
+import numpy as np
+import argparse
+import librosa
+import matplotlib.pyplot as plt
+import torch
+
+from utils.utilities import create_folder, get_filename
+from pytorch.models import *
+from pytorch.pytorch_utils import move_data_to_device
+import utils.config as source_config
+
+def get_arg():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--sample_rate', type=int, default=16000)
+    parser.add_argument('--window_size', type=int, default=512)
+    parser.add_argument('--hop_size', type=int, default=160)
+    parser.add_argument('--mel_bins', type=int, default=64)
+    parser.add_argument('--fmin', type=int, default=50)
+    parser.add_argument('--fmax', type=int, default=8000) 
+    parser.add_argument('--model_type', type=str, required=True)
+    parser.add_argument('--checkpoint_path', type=str, required=True)
+    parser.add_argument('--audio_path', type=str, required=True)
+    parser.add_argument('--cuda', action='store_true', default=False)
+
+    args = parser.parse_args()
+    return args
+
+def audio_tagging(args):
+    # Arugments & parameters
+    sample_rate = args.sample_rate
+    window_size = args.window_size
+    hop_size = args.hop_size
+    mel_bins = args.mel_bins
+    fmin = args.fmin
+    fmax = args.fmax
+    model_type = args.model_type
+    checkpoint_path = args.checkpoint_path
+    audio_path = args.audio_path
+    device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
+    
+    classes_num = source_config.classes_num
+    labels = source_config.labels
+
+    Model = eval(model_type)
+    model = Model(sample_rate=sample_rate, window_size=window_size, 
+                  hop_size=hop_size, mel_bins=mel_bins, fmin=fmin, fmax=fmax, 
+                  classes_num=classes_num)
+    
+    checkpoint = torch.load(checkpoint_path, map_location=device)
+    model.load_state_dict(checkpoint['model'])
+
+    # Parallel
+    if 'cuda' in str(device):
+        model.to(device)
+        print('GPU number: {}'.format(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model)
+    else:
+        print('Using CPU.')
+
+    # Load audio
+    (waveform, _) = librosa.core.load(audio_path, sr=sample_rate, mono=True)
+
+    # Forward
+    with torch.no_grad():
+        model.eval()
+        batch_output_dict = model(waveform, None)
+    
+    clipwise_output = batch_output_dict['clipwise_output'].data.cpu().numpy()[0]
+    """(classes_num,)"""
+
+    sorted_indexes = np.argsort(clipwise_output)[::-1]
+
+    for k in range(10):
+        print('{}: {:.3f}'.format(np.array(labels)[sorted_indexes[k]], 
+            clipwise_output[sorted_indexes[k]]))
+    
+    if 'embedding' in batch_output_dict.keys():
+        embedding = batch_output_dict['embedding'].data.cpu().numpy()[0]
+        print('embedding: {}'.format(embedding.shape))
+    
+    return clipwise_output, labels
+
+if __name__ == '__main__':
+    args = get_arg()
+
+    audio_tagging(args)
+
